3632c36bbdbeea7da44ea8cbba75cece22f0bdd2
Vishesh Gupta <81924917+visheshGupta01@users.noreply.github.com>
2024-06-07 14:52:16 +0530

    Update firebaseconfig.js

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth,setPersistence,browserLocalPersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP85AOAOQgZ5Q04MovAU539Wc9vmUD63M",
-  authDomain: "todo-app-82c79.firebaseapp.com",
-  projectId: "todo-app-82c79",
-  storageBucket: "todo-app-82c79.appspot.com",
-  messagingSenderId: "1092260152487",
-  appId: "1:1092260152487:web:455f581b5dfedf66f48782",
-  measurementId: "G-0C1NSCDGKK"
+  apiKey: "AIzaSyAgnkkLUmrW_aeYMEJiV53cCp4RzdOrcnI",
+  authDomain: "todo-app-85307.firebaseapp.com",
+  projectId: "todo-app-85307",
+  storageBucket: "todo-app-85307.appspot.com",
+  messagingSenderId: "442139363753",
+  appId: "1:442139363753:web:b9f853feebad6343095cb8"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
